1208b5b1db04155ff295a48f44d749229f75cc1d
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-02-15 21:27:51 +0000

    set full width and title

---
+++ src/python_labour_app/analytics/query_console.py
@@ -0,0 +1,13 @@
+import marimo
+
+__generated_with = "0.19.11"
+app = marimo.App(width="full", app_title="Qquey Console")
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
