63ad750f2cf91afed84283ea0636820d2d93ebe4
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-02-15 21:29:32 +0000

    add title

--- src/python_labour_app/analytics/query_console.py
+++ src/python_labour_app/analytics/query_console.py
@@ -4,9 +4,19 @@ __generated_with = "0.19.11"
 app = marimo.App(width="full", app_title="Qquey Console")
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    # SQL Query Console
+    """)
+    return
+
+
 @app.cell
 def _():
-    return
+    import marimo as mo
+
+    return (mo,)
 
 
 if __name__ == "__main__":
