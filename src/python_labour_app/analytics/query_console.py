f812e4840f2358a616be874bcd5e94d3487c82bd
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-02-15 21:41:40 +0000

    connect to postgresql

--- src/python_labour_app/analytics/query_console.py
+++ src/python_labour_app/analytics/query_console.py
@@ -18,7 +18,20 @@ def _():
     import os
     import sqlalchemy
 
-    return (mo,)
+    return mo, os, sqlalchemy
+
+
+@app.cell
+def _(os, sqlalchemy):
+    # Vonnect to postgres
+    _password = os.environ.get("POSTGRES_PASSWORD")
+    _username = os.environ.get("POSTGRES_USER")
+    _database = os.environ.get("POSTGRES_DB")
+    # postgresql to use psycopg2, posgresql+psycopg to use psycopg3
+
+    DATABASE_URL = f"postgresql+psycopg://{_username}:{_password}@db:5432/{_database}"
+    engine = sqlalchemy.create_engine(DATABASE_URL)
+    return
 
 
 if __name__ == "__main__":
