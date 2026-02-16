f5c1667a9d8bdbcba819b945506f453084e87a84
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-02-16 23:00:40 +0000

    convert comment to docstring

--- src/python_labour_app/db/base.py
+++ src/python_labour_app/db/base.py
@@ -1,8 +1,9 @@
-# All models import from the same Base.
-# Allows separation of concerns, scaling and prevents circular imports.
-
 from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
+    """
+    All models import from the same Base.
+    Allows separation of concerns, scaling and prevents circular imports.
+    """
     pass
