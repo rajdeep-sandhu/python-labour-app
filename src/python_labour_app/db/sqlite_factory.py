b2cb95b6e44c54e3a8bd722e357e7000ee51d9fd
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-02-21 17:32:40 +0000

    add __init__()

--- src/python_labour_app/db/sqlite_factory.py
+++ src/python_labour_app/db/sqlite_factory.py
@@ -9,3 +9,10 @@ from .database_factory import DatabaseFactory
 
 class SQLiteFactory(DatabaseFactory):
     """Concrete sqlite factory."""
+
+    def __init__(self, url: str = "sqlite+pysqlite:///:memory:") -> None:
+        self._engine = create_engine(url)
+
+        self._sessionmaker = sessionmaker(
+            bind=self._engine, autoflush=False, autocommit=False
+        )
