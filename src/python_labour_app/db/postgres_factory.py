3a88c5be05a79ef4207a3186457618d097ea2b68
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-02-21 11:15:16 +0000

    create __init__()

---
+++ src/python_labour_app/db/postgres_factory.py
@@ -0,0 +1,34 @@
+import os
+
+from sqlalchemy import URL, create_engine
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import Session, sessionmaker
+
+from .database_factory import DatabaseFactory
+
+
+class PostgresFactory(DatabaseFactory):
+    """Concrete postgres factory."""
+
+    def __init__(self) -> None:
+        self._username = os.environ["POSTGRES_USER"]
+        self._password = os.environ["POSTGRES_PASSWORD"]
+        self._database = os.environ["POSTGRES_DB"]
+        self._host = os.environ.get("POSTGRES_HOST", "localhost")
+        self._port = 5432
+
+        # postgresql to use psycopg2, posgresql+psycopg to use psycopg3
+        url = URL.create(
+            drivername="postgresql+psycopg",
+            username=self._username,
+            password=self._password,
+            host=self._host,
+            port=self._port,
+            database=self._database,
+        )
+
+        self._engine = create_engine(url)
+
+        self._session = sessionmaker(
+            bind=self._engine, autoflush=False, autocommit=False
+        )
