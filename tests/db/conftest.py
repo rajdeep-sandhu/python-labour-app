68cbee6739438a56758fa76cdd401712c0f375a3
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-02-22 01:49:38 +0000

    add fixture engine()

--- tests/db/conftest.py
+++ tests/db/conftest.py
@@ -8,3 +8,17 @@ from python_labour_app.db.sqlite_factory import SQLiteFactory
 @pytest.fixture(scope="session")
 def sqlite_factory() -> SQLiteFactory:
     return SQLiteFactory(url="sqlite+pysqlite:///test.db")
+
+
+@pytest.fixture(scope="session")
+def engine(sqlite_factory: SQLiteFactory):
+    """Setup engine and metadata for the test session."""
+    engine = sqlite_factory.create_engine()
+
+    # Setup: Create tables.
+    Base.metadata.create_all(bind=engine)
+
+    yield
+    
+    # Teardown: Drop tables.
+    Base.metadata.drop_all(bind=engine)
