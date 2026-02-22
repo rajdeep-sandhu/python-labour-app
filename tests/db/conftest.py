319f09218d6ffc84f1a92a9643e9c84ddb4dac45
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-02-22 16:44:45 +0000

    remove fixture sqlite_factory()

--- tests/db/conftest.py
+++ tests/db/conftest.py
@@ -7,11 +7,6 @@ from sqlalchemy.orm import Session, sessionmaker
 from python_labour_app.db.models import Base
 from python_labour_app.db.sqlite_factory import SQLiteFactory
 
-@pytest.fixture(scope="session")
-def sqlite_factory() -> SQLiteFactory:
-    """Return SQLiteFactory for a test database."""
-    return SQLiteFactory(url="sqlite+pysqlite:///test.db")
-
 
 @pytest.fixture(scope="session")
 def engine() -> Generator[Engine, None, None]:
