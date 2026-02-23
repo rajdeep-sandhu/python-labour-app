55651ac73d1ef97f30544041fcc304acbdf08811
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-02-23 22:23:52 +0000

    add PostgresFactory and SQLiteFactory to imports.

--- src/python_labour_app/db/factories/__init__.py
+++ src/python_labour_app/db/factories/__init__.py
@@ -1,4 +1,6 @@
 # db/factories/__init__.py
 from .database_factory import DatabaseFactory
+from .postgres_factory import PostgresFactory
+from .sqlite_factory import SQLiteFactory
 
-__all__ = ["DatabaseFactory"]
+__all__ = ["DatabaseFactory", "PostgresFactory", "SQLiteFactory"]
