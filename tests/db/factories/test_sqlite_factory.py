119d996c7792b5055550a9629fc4220c002daabf
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-02-24 23:03:12 +0000

    add type annotations to test_sqlite_factory_creates_engine()

--- tests/db/factories/test_sqlite_factory.py
+++ tests/db/factories/test_sqlite_factory.py
@@ -6,8 +6,8 @@ from python_labour_app.db.factories.sqlite_factory import SQLiteFactory
 
 
 def test_sqlite_factory_creates_engine() -> None:
-    factory = SQLiteFactory(url="sqlite+pysqlite:///test.db")
-    engine = factory.create_engine()
+    factory: SQLiteFactory = SQLiteFactory(url="sqlite+pysqlite:///test.db")
+    engine: Engine = factory.create_engine()
 
     assert isinstance(engine, Engine)
     assert "sqlite" in str(engine.url)
