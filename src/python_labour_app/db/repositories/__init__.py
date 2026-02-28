0a7a80eed7d139893977fd6ec6320ba7be23c3cb
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-02-28 21:33:21 +0000

    add import for class Repository

--- src/python_labour_app/db/repositories/__init__.py
+++ src/python_labour_app/db/repositories/__init__.py
@@ -1,4 +1,5 @@
 # db/repositories/__init__.py
 from .employee_repository import EmployeeRepository
+from .repository import Repository
 
-__all__ = ["EmployeeRepository"]
+__all__ = ["Repository", "EmployeeRepository"]
