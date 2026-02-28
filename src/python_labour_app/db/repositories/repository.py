7b3b5fbf70e0c1e40be715597794c5c3f71d27b8
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-02-28 21:15:54 +0000

    add get_all()

--- src/python_labour_app/db/repositories/repository.py
+++ src/python_labour_app/db/repositories/repository.py
@@ -1,9 +1,15 @@
 # repository.py
 from abc import ABC, abstractmethod
+from typing import Generator
+
 
 class Repository[T](ABC):
     @abstractmethod
     def get(self, id: int) -> T:
         """Get item by id."""
         raise NotImplementedError
-    
+
+    @abstractmethod
+    def get_all(self) -> Generator[T, None, None]:
+        """Get all items."""
+        raise NotImplementedError
