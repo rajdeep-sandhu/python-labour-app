aaa2ade523384f434ac491f143217056045f711b
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-03-08 22:49:26 +0000

    add .get().

--- src/python_labour_app/db/repositories/employee_repository.py
+++ src/python_labour_app/db/repositories/employee_repository.py
@@ -1,8 +1,17 @@
 # employee_repository.py
+from typing import Generator
 
 from sqlalchemy.orm import Session
+
+from python_labour_app.db.models import Employee
+from python_labour_app.db.repositories.repository import Repository
 
 
 class EmployeeRepository:
     def __init__(self, session: Session) -> None:
         self._session = session
+
+    def get(self, id: int) -> Employee | None:
+        """Get an employee by id."""
+        employee: Employee | None = self._session.get(Employee, id)
+        return employee
