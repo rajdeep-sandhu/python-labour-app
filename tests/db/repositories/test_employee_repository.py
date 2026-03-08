bf5628668c736e93e47e74ff944815d292a9a56d
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-03-08 22:11:06 +0000

    remove self from test_get_returns_employee_by_id()

--- tests/db/repositories/test_employee_repository.py
+++ tests/db/repositories/test_employee_repository.py
@@ -5,7 +5,7 @@
 from python_labour_app.db.models.employee import Employee
 
 
-def test_get_returns_employee_by_id(self, employee_repo, mock_session):
+def test_get_returns_employee_by_id(employee_repo, mock_session):
     employee = Employee(id=1, emp_no=42, first_name="Baba", last_name="Dook")
     mock_session.first_result = employee
 
