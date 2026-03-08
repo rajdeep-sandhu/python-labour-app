49b103b4ea37022973a472fb260d76a0bd321de8
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-03-08 21:30:17 +0000

    add .query()

--- tests/db/repositories/conftest.py
+++ tests/db/repositories/conftest.py
@@ -1,10 +1,13 @@
 # repositories/conftest.py
+from typing import Self
+
 import pytest
 
 
 @pytest.fixture
 def mock_session(monkeypatch) -> None:
     """Create a mock session object."""
+
     class MockSession:
         def __init__(self) -> None:
             self.added = []
@@ -14,4 +17,7 @@ def mock_session(monkeypatch) -> None:
             self.filter_result = None
             self.first_result = None
             self.all_result = None
-    
+
+        def query(self, model) -> Self:
+            self.query_result = model
+            return self
