f9d2d7a4e419528281dd8e50de730e0f2929c5c1
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-03-08 18:43:09 +0000

    add .__init__()

--- tests/db/repositories/conftest.py
+++ tests/db/repositories/conftest.py
@@ -6,5 +6,12 @@ import pytest
 def mock_session(monkeypatch) -> None:
     """Create a mock session object."""
     class MockSession:
-        raise NotImplementedError
+        def __init__(self) -> None:
+            self.added = []
+            self.committed = False
+            self.deleted = []
+            self.query_result = None
+            self.filter_result = None
+            self.first_result = None
+            self.all_result = None
     
