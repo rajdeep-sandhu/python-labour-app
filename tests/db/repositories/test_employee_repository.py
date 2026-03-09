4c1c5e493422a0f1362ce3ae69801ac15d2516fb
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-03-09 10:09:26 +0000

    refactor test_get_returns_employee_by_id() to add result variable

--- tests/db/repositories/test_employee_repository.py
+++ tests/db/repositories/test_employee_repository.py
@@ -6,7 +6,8 @@ from python_labour_app.db.models.employee import Employee
 
 
 def test_get_returns_employee_by_id(employee_repo, mock_session):
-    employee = Employee(id=1, emp_no=42, first_name="Baba", last_name="Dook")
+    employee: Employee = Employee(id=1, emp_no=42, first_name="Baba", last_name="Dook")
     mock_session.first_result = employee
+    result: Employee = employee_repo.get(1)
 
-    assert employee_repo.get(1) == employee
+    assert result == employee
