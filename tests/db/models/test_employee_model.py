eb5f9c5b7b01c49e1e688032649b5024a200b672
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-03-11 16:58:27 +0000

    refactor to replace session with sqlite_session

--- tests/db/models/test_employee_model.py
+++ tests/db/models/test_employee_model.py
@@ -4,49 +4,49 @@ from sqlalchemy.exc import IntegrityError
 from python_labour_app.db.models import Employee
 
 
-def test_employee_persists(session):
+def test_employee_persists(sqlite_session):
     employee: Employee = Employee(first_name="John", last_name="Doe")
-    session.add(employee)
-    session.commit()
-    session.refresh(employee)
+    sqlite_session.add(employee)
+    sqlite_session.commit()
+    sqlite_session.refresh(employee)
 
     assert employee.id is not None
     assert employee.emp_no is None
     assert employee.is_active is True
 
 
-def test_employee_requires_first_name(session) -> None:
+def test_employee_requires_first_name(sqlite_session) -> None:
     employee: Employee = Employee(last_name="Doe")
-    session.add(employee)
+    sqlite_session.add(employee)
 
     with pytest.raises(IntegrityError):
-        session.commit()
+        sqlite_session.commit()
 
 
-def test_employee_requires_last_name(session) -> None:
+def test_employee_requires_last_name(sqlite_session) -> None:
     employee: Employee = Employee(first_name="John")
-    session.add(employee)
+    sqlite_session.add(employee)
 
     with pytest.raises(IntegrityError):
-        session.commit()
+        sqlite_session.commit()
 
 
-def test_emp_no_unique_constraint(session) -> None:
+def test_emp_no_unique_constraint(sqlite_session) -> None:
     employee_1: Employee = Employee(first_name="John", last_name="Doe", emp_no=5)
     employee_2: Employee = Employee(first_name="Ruchika", last_name="Yashpal", emp_no=5)
-    session.add_all([employee_1, employee_2])
+    sqlite_session.add_all([employee_1, employee_2])
 
     with pytest.raises(IntegrityError):
-        session.commit()
+        sqlite_session.commit()
 
 
-def test_employee_can_be_deactivated(session) -> None:
+def test_employee_can_be_deactivated(sqlite_session) -> None:
     employee: Employee = Employee(first_name="Amrik", last_name="Singh")
-    session.add(employee)
-    session.commit()
+    sqlite_session.add(employee)
+    sqlite_session.commit()
 
     employee.is_active = False
-    session.commit()
-    session.refresh(employee)
+    sqlite_session.commit()
+    sqlite_session.refresh(employee)
 
     assert employee.is_active is False
