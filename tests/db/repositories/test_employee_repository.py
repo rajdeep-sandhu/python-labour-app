a5dd000caa8d45b624995c7295aa628e2bc3f9ad
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-03-11 20:14:12 +0000

    add type annotation to test_get_all_returns_all_employees()

--- tests/db/repositories/test_employee_repository.py
+++ tests/db/repositories/test_employee_repository.py
@@ -41,7 +41,7 @@ def test_get_all_returns_all_employees(sqlite_session):
 
     sqlite_session.commit()
 
-    repo = EmployeeRepository(session=sqlite_session)
+    repo: EmployeeRepository = EmployeeRepository(session=sqlite_session)
     result: list[Employee] = list(repo.get_all())
 
     assert len(result) == 3
