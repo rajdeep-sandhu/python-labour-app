3cb3e6791a52ff86c35ed1e606f5053391f94564
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-03-11 13:46:43 +0000

    refactor test_get_returns_none_if_id_not_exists() to use sqlite_session instead of mock_session

--- tests/db/repositories/test_employee_repository.py
+++ tests/db/repositories/test_employee_repository.py
@@ -17,11 +17,13 @@ def test_get_returns_employee_by_id(employee_repo, sqlite_session):
     assert result == employee
 
 
-def test_get_returns_none_if_id_not_exists(employee_repo, mock_session):
+def test_get_returns_none_if_id_not_exists(employee_repo, sqlite_session):
     employee: Employee = Employee(
         id=1, emp_no=42, is_active=True, first_name="Baba", last_name="Dook"
     )
-    mock_session.get_results[Employee] = {1: employee}
+    sqlite_session.add(employee)
+    sqlite_session.commit()
+
     result: Employee = employee_repo.get(2)
 
     assert result is None
